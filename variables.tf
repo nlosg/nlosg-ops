b31dc728c3283e75193aa0a7be95f079bc57f21d
Noel <noel.osagie@devoteam.com>
2023-10-08 17:49:00 +0100

    updates vars

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "gcp_wi_project" {
 	type = string
+  default = ""
 }
 
 variable "gcp_org_id" {
   type = string
+  default = ""
 }
 
 variable "tfc_wi_pool" {
   type = string
+  default = ""
 }
 
 variable "tfc_service_account" {
   type = string
+  default = ""
 }
