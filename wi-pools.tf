615830046226082dcb8a486de8865593504ed91f
Noel <noel.osagie@devoteam.com>
2023-10-08 18:09:12 +0100

    update wi

--- wi-pools.tf
+++ wi-pools.tf
@@ -1,8 +1,14 @@
 module "wi-pools" {
 	source = "./modules/wip"
 
+	tfc_hostname = var.tfc_hostname
+	tfc_project_name = "nlosg-banking"
+    tfc_workspace_name = "nlosg-banking-dev"
 	gcp_wi_project = "nlosg-wi-dev"
 	gcp_org_id = "519726315968"
 	tfc_wi_pool = "nlosg-wi-dev"
+	workload_identity_pool_provider_id = "nlosg-wi-dev"
 	tfc_service_account = "tf-sa-dev"
+	
+
 }
