e5c6aa471e3937926d8779f52c67d7587bcda470
Noel <noel.osagie@devoteam.com>
2023-10-08 17:31:58 +0100

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "gcp_wi_project" {
+	type = string
+}
+
+variable "gcp_org_id" {
+  type = string
+}
+
+variable "tfc_wi_pool" {
+  type = string
+}
+
+variable "tfc_service_account" {
+  type = string
+}
