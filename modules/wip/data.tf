9b572388df766ba86ec4be484ab5c72abd7d720e
Noel <noel.osagie@devoteam.com>
2023-10-08 20:59:55 +0100

    update tfc config

--- modules/wip/data.tf
+++ modules/wip/data.tf
@@ -1,8 +1,3 @@
 data "tfe_organization" "tfc_org_name" {
   name = "nlosg-utokan"
 }
-
-data "tfe_oauth_client" "client" {
-  organization = "mnlosg-utokan"
-  name         = "tfc_github_oauth_client"
-}
