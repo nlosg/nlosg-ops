ca55d2f2f6a7bd22fe3c1421fd6e53f50f0a8de3
Noel <noel.osagie@devoteam.com>
2023-10-09 09:21:28 +0100

    add sa prj

--- modules/wip/outputs.tf
+++ modules/wip/outputs.tf
@@ -1,3 +1,3 @@
 output "wi_pool_name" {
-	value = google_service_account_iam_member.tfc_project_member.member
+	value = google_iam_workload_identity_pool.tfc_pool.name
 }
--- sa-projects.tf
+++ sa-projects.tf
@@ -4,5 +4,5 @@ module "sa-xyz" {
   sa_project          = "nlosg-xyz-dev"
   prj_service_account = "nlosg-xyz-dev"
   gcp_org_id          = "519726315968"
-  principal           = module.wi-pool-xyz.wi_pool_name
+  principal           = "principalSet://iam.googleapis.com/${module.wi-pool-xyz.wi_pool_name}/*"
 }
