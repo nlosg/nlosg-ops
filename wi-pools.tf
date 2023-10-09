0beb1b36873fe88065c0d80564749d7662f60894
nlosg-utokan <n.osagie@live.co.uk>
2023-10-09 16:10:19 +0100

    Update wi-pools.tf

--- wi-pools.tf
+++ wi-pools.tf
@@ -14,3 +14,20 @@ module "wi-pool-xyz" {
   GITHUB_TOKEN                       = var.GITHUB_TOKEN
   # folder_id = "nlosgg dev"
 }
+
+module "wi-pool-abc" {
+  source = "./modules/wip"
+
+  tfc_hostname                       = var.tfc_hostname
+  tfc_project_name                   = "nlosg-abc"
+  tfc_workspace_name                 = "nlosg-abc"
+  gcp_wi_project                     = "nlosg-wi-abc"
+  gcp_org_id                         = "519726315968"
+  tfc_wi_pool                        = "nlosg-wi-abc"
+  workload_identity_pool_provider_id = "nlosg-wi-abc"
+  tfc_service_account                = "sa-tf-nlosg-abc"
+  github_repository                  = "nlosg-abc"
+  branch                             = var.branch
+  GITHUB_TOKEN                       = var.GITHUB_TOKEN
+  # folder_id = "nlosgg dev"
+}
