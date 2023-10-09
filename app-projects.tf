0a2f3410633c42f7337a3502e063cee631eef70f
Noel <noel.osagie@devoteam.com>
2023-10-09 12:27:12 +0100

    add permission

--- app-projects.tf
+++ app-projects.tf
@@ -2,6 +2,6 @@ module "nlosg-app-xyz" {
   source = "./modules/app-projects"
 
   app_project     = "prj-nlosg-xyz-a"
-  service_account = module.sa-xyz.prj_service_account
+  service_account = "serviceAccount:${module.sa-xyz.prj_service_account}"
   gcp_org_id      = "519726315968"
 }
