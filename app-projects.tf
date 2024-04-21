f8d86521d27e7a8cd006950fff655cc881dff795
Noel <noel.osagie@devoteam.com>
2024-04-21 10:40:09 +0100

    comment out xyz app

--- app-projects.tf
+++ app-projects.tf
@@ -1,7 +1,7 @@
-module "nlosg-app-xyz" {
+/*module "nlosg-app-xyz" {
   source = "./modules/app-projects"
 
   app_project     = "prj-nlosg-xyz-a"
   service_account = "serviceAccount:${module.sa-xyz.prj_service_account}"
   gcp_org_id      = "519726315968"
-}
+}*/
--- sa-projects.tf
+++ sa-projects.tf
@@ -1,8 +1,8 @@
-module "sa-xyz" {
+/*module "sa-xyz" {
   source = "./modules/sa-projects"
 
   sa_project          = "nlosg-xyz-dev"
   prj_service_account = "nlosg-xyz-dev"
   gcp_org_id          = "519726315968"
   principal           = "principalSet://iam.googleapis.com/${module.wi-pool-xyz.wi_pool_name}/*"
-}
+}*/
